7a1c167a7ce5ba2057b880a2ca38d5e5d0a0f31d
BotMeki <75749558+BotMeki@users.noreply.github.com>
2020-12-15 20:23:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ const arrayBulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Jul
 const bulan = arrayBulan[moment().format('MM') - 1]
 
 const config = {
-    A187: '👾AR15BOT👾',
+    A187: '👾BotMeki👾',
     instagram: 'https://instagram.com/_sadboy.ig',
-    nomer: 'wa.me/6285722553839',
+    nomer: 'wa.me/6285591654034',
     aktif: '24 JAM',
     youtube: 'https://youtube.com/channel/UCGYLWtyT9IADYNUiK0uZiGg',
     whatsapp: 'https://chat.whatsapp.com/DSSHmG2KjKJLoFp9B9mkVs',
